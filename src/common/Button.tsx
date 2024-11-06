9f97ec022e3a4ba7e77130bb85d61a5de8263430
Agata Kamińska <delicadency@gmail.com>
2024-11-06 20:56:21 +0000

    feat: created interface for Button component, added style variants

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -1,16 +1,34 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
-export const Button = ({ type, className, onClick, children, href }) => {
+type ButtonVariant = "transparent" | "lime" | "black";
+
+interface ButtonProps {
+  type?: string,
+  className: string,
+  onClick: (event: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => void,
+  children: ReactNode,
+  href: string,
+  variant: ButtonVariant,
+}
+
+export const Button:React.FC<ButtonProps> = ({ type, className, onClick, children, href, variant }) => {
+  const variantClasses = {
+    transparent: "bg-transparent text-black border-black hover:bg-white hover:text-black",
+    lime: "bg-lime text-black border-lime hover:bg-black hover:text-white",
+    black: "bg-black text-white border-black hover:bg-lime hover:text-black"
+  }
+
   if (type === "submit") {
     return (
-      <button type="submit" className={className} onClick={onClick}>
+      <button type="submit" className={className + variant} onClick={onClick}>
         {children}
       </button>
     );
   }
   return (
     <Link href={href} passHref>
-      <a className={className}></a>
+      <a className={className} onClick={onClick}>{ children }</a>
     </Link>
   );
 };
